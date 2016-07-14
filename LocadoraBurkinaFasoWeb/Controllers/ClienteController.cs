d655096a3e6b4ccc6e9bb10bac01c43c2b38a8b7
Mateus <arrudamateus3@hotmail.com>
2016-07-14 18:53:20 -0300

    Validação

--- LocadoraBurkinaFasoWeb/Controllers/ClienteController.cs
+++ LocadoraBurkinaFasoWeb/Controllers/ClienteController.cs
@@ -5,14 +5,12 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LocadoraBurkinaFasoWeb.Controllers
 {
     public class ClienteController : Controller
     {
-
-        //Primeira Requisição a Página, e Aparição do Formulário.
-
         public ActionResult Criar_Cliente()
         {
             var cliente = new Cliente();
@@ -34,110 +32,18 @@ namespace LocadoraBurkinaFasoWeb.Controllers
                 ModelState.AddModelError("", "Por favor escolha uma data inferior a " + DateTime.Now);
             }
 
-            var Telefone_valido = Validar_Telefone(cliente.Telefone);
             var CPF_valido = Valida_CPF(cliente.CPF);
 
-
-            if (ModelState.IsValid && validar_data && Telefone_valido && CPF_valido)
+            if (ModelState.IsValid && validar_data && CPF_valido)
             {
-                cliente.Telefone = cliente.Telefone.Insert(0, "(");
-                cliente.Telefone = cliente.Telefone.Insert(3, ")");
-                cliente.Telefone = cliente.Telefone.Insert(4, " ");
-                cliente.Telefone = cliente.Telefone.Insert(9, "-");
-
                 cliente.CPF = cliente.CPF.Insert(3, ".");
                 cliente.CPF = cliente.CPF.Insert(7, ".");
                 cliente.CPF = cliente.CPF.Insert(11, "-");
 
-
-                return View("Criado_Cliente", cliente);
+                return View("Index", "Home");
             }
 
             return View("Criar_Cliente");
-        }
-
-
-
-        //Validação de Telefones Brasileiros
-
-        [HttpPost]
-        public bool Validar_Telefone(string Telefone_do_Cliente)
-        {
-            var verificador_de_numeros = 0;
-            char[] vetor_de_caractere;
-
-            try
-            {
-                vetor_de_caractere = Telefone_do_Cliente.ToCharArray();
-            }
-            catch
-            {
-                return false;
-            }
-
-            for (int i = 0; i < Telefone_do_Cliente.Length; i++)
-            {
-                if (char.IsLetter(vetor_de_caractere[i]))
-                {
-                    verificador_de_numeros++;
-                }
-            }
-
-            if (verificador_de_numeros > 0)
-            {
-                ModelState.AddModelError("", "Telefone não possui letras");
-                return false;
-            }
-
-            else
-            {
-                // Se não possuir letras
-                //Retira o 55 se estiver na posição 0 e 1.
-
-                if (Telefone_do_Cliente[0] == '5' && Telefone_do_Cliente[1] == '5')
-                {
-                    Telefone_do_Cliente = Telefone_do_Cliente.Remove(0, 2);
-                }
-
-                Telefone_do_Cliente = Telefone_do_Cliente.Replace("(", "");
-                Telefone_do_Cliente = Telefone_do_Cliente.Replace(")", "");
-                Telefone_do_Cliente = Telefone_do_Cliente.Replace(" ", "");
-                Telefone_do_Cliente = Telefone_do_Cliente.Replace("-", "");
-
-                var Telefone = Telefone_do_Cliente;
-                var condicao_telefone1 = Telefone.Length == 11;
-                var condicao_telefone2 = Telefone.Length == 10;
-
-
-                //Se o telefone não tiver 11 (Celular) ou 10 (Fixo) Digitos ele dara erro. (21) 98617 2746
-
-                if (!condicao_telefone1 && !condicao_telefone2)
-                {
-                    ModelState.AddModelError("", "Número de Telefone Inválido");
-                    return false;
-                }
-
-                int[] vetor_inteiro_telefone = new int[Telefone.Length];
-
-                for (int i = 0; i < Telefone.Length; i++)
-                {
-                    vetor_inteiro_telefone[i] = int.Parse(Telefone[i].ToString());
-                }
-
-                if (vetor_inteiro_telefone.Length == 11 && vetor_inteiro_telefone[2] == 9)
-                {
-                    var validar_Telefone_Celular = vetor_inteiro_telefone[0] >= 2;
-                    return validar_Telefone_Celular;
-                }
-
-                else if (vetor_inteiro_telefone.Length == 10 && vetor_inteiro_telefone[2] != 9)
-                {
-                    var validar_Telefone_Fixo = vetor_inteiro_telefone[0] >= 2 && vetor_inteiro_telefone[2] >= 2;
-                    return validar_Telefone_Fixo;
-                }
-                ModelState.AddModelError("", "Telefone Inválido");
-                return false;
-            }
         }
 
         //Validação de CPF.
@@ -147,33 +53,8 @@ namespace LocadoraBurkinaFasoWeb.Controllers
         {
             var CPF = CPF_do_Cliente;
 
-            try
-            {
                 CPF = CPF.Replace(".", "");
                 CPF = CPF.Replace("-", "");
-            }
-            catch
-            {
-                return false;
-            }
-
-            var vetor_caracteres_CPF = CPF.ToCharArray();
-            var verificador_de_numeros = 0;
-
-
-            for (int i = 0; i < CPF.Length; i++)
-            {
-                if (char.IsLetter(vetor_caracteres_CPF[i]))
-                {
-                    verificador_de_numeros++;
-                }
-            }
-
-            if (verificador_de_numeros > 0)
-            {
-                ModelState.AddModelError("", "CPF não possuem letras");
-                return false;
-            }
 
             //Verifica se o CPF tem 11 digitos.
 
@@ -185,7 +66,6 @@ namespace LocadoraBurkinaFasoWeb.Controllers
                 {
                     CPF_em_inteiros[i] = int.Parse(CPF[i].ToString());
                 }
-
 
                 var maximo = CPF_em_inteiros.Max();
                 var minimo = CPF_em_inteiros.Min();
@@ -251,10 +131,11 @@ namespace LocadoraBurkinaFasoWeb.Controllers
             return false;
         }
 
-        //Se todo o Formulário estiver preenchido corretamente ele executa essa Action
-
         public ActionResult Criado_Cliente(Cliente cliente)
         {
+
+            string userID = Membership.GetUser().ProviderUserKey.ToString();
+
             return View(cliente);
         }
     }
--- LocadoraBurkinaFasoWeb/Models/Cliente.cs
+++ LocadoraBurkinaFasoWeb/Models/Cliente.cs
@@ -11,8 +11,6 @@ namespace LocadoraBurkinaFasoWeb.Models
     public class Cliente
     {
 
-        //Expressões Regulares (Validações de campo por Regex, o JS fara o trabalho no lado do Cliente.)
-
         [Required]
         [DisplayName("Nome")]
         [RegularExpression(@"^([a-zA-Z_\s]+)$", ErrorMessage = "Nome Inválido")]
@@ -23,24 +21,22 @@ namespace LocadoraBurkinaFasoWeb.Models
         [RegularExpression(@"^(RUA|Rua|R.|AVENIDA|Avenida|AV.|TRAVESSA|Travessa|TRAV.|Trav.) ([a-zA-Z_\s]+), ([0-9]{0,})", ErrorMessage = "Modelo de Endereço: Rua Batata da Neve , 100")]
         public string Endereco { get; set; }
 
-        /*Sem Expressões Regulares Quando o User mandar a requesição
-         pro servidor, ele mesmo fara as validações. OK*/
-
         [Required]
-        [Phone(ErrorMessage = "Digite um número de Telefone Válido")]
+        [RegularExpression(@"^(([0-9]{1})*[- .(]*([0-9a-zA-Z]{3})*[- .)]*[0-9a-zA-Z]{3}[- .]*[0-9a-zA-Z]{4})+$|^\(?\d{2}\)?[\s-]?\d{4}-?\d{4}$", ErrorMessage = "Número Inválido.")]
         public string Telefone { get; set; }
 
+        [Required(ErrorMessage = "CPF é obrigatório")]
+        [RegularExpression(@"^\d*[0-9]{3}(\.\d*[0-9]{3}\.\d*[0-9]{3}\-\d*[0-9]{2})?$", ErrorMessage = "CPF Inválido")]
+        [StringLength(14,MinimumLength =11,ErrorMessage ="O campo CPF tem no mínimo 11 digitos ")]
+        public string CPF { get; set; }
 
         [Required(ErrorMessage = "Digite sua Data de Nascimento")]
         [DisplayName("Data de Nascimento")]
         [DisplayFormat(ApplyFormatInEditMode = true)]
         public DateTime? Data_de_Nascimento { get; set; }
 
-
-        [Required(ErrorMessage = "CPF é obrigatório")]
-        public string CPF { get; set; }
-
         [Required(ErrorMessage = "Selecione o seu gênero")]
         public string Sexo { get; set; }
+
     }
 }
